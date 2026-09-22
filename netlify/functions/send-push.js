6c02fa0516df11e99643d50446c320fddba3bc67
HeikoRegg <Heiko_schlegel@web.de>
2026-09-22 23:20:37 +0200

    Add files via upload

--- netlify/functions/send-push.js
+++ netlify/functions/send-push.js
@@ -7,7 +7,7 @@
 import webpush from "web-push";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY);
 
 webpush.setVapidDetails(
   process.env.VAPID_SUBJECT || "mailto:kein-kontakt@example.com",
